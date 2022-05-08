36b752b13c940d428768ba4ab39e59790a045f24
roshani226 <86221628+roshani226@users.noreply.github.com>
2022-05-08 11:50:36 +0530

    Update userCtrl.js
    userctrl.js

--- backend/Controllers/userCtrl.js
+++ backend/Controllers/userCtrl.js
@@ -11,3 +11,25 @@ const client = new OAuth2(process.env.MAILING_SERVICE_CLIENT_ID)
 
 const {CLIENT_URL} = process.env
 
+const userCtrl = {
+    register: async (req, res) => {
+        try {
+            const {name, email, phonenumber, dateofbirth, password} = req.body
+            
+            if(!name || !email || !password)
+                return res.status(400).json({msg: "Please fill in all fields."})
+
+            if(!validateEmail(email))
+                return res.status(400).json({msg: "Invalid emails."})
+
+            const user = await Users.findOne({email})
+            if(user) return res.status(400).json({msg: "This email already exists."})
+
+            if(password.length < 6)
+                return res.status(400).json({msg: "Password must be at least 6 characters."})
+
+            const passwordHash = await bcrypt.hash(password, 12)
+
+            const newUser = {
+                name, email, phonenumber, dateofbirth, password: passwordHash
+            }
