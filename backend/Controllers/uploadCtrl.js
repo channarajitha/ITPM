580b3922f0339bb65b504c404b4cc3596c8f9473
roshani226 <86221628+roshani226@users.noreply.github.com>
2022-05-08 11:48:44 +0530

    Update uploadCtrl.js
    uploadctrl.js

--- backend/Controllers/uploadCtrl.js
+++ backend/Controllers/uploadCtrl.js
@@ -9,3 +9,24 @@ cloudinary.config({
 
 
 
+const uploadCtrl = {
+    uploadAvatar: (req, res) => {
+        try {
+            const file = req.files.file;
+            
+            cloudinary.v2.uploader.upload(file.tempFilePath, {
+                folder: 'avatar', width: 150, height: 150, crop: "fill"
+            }, async(err, result) => {
+                if(err) throw err;
+
+                removeTmp(file.tempFilePath)
+
+                res.json({url: result.secure_url})
+            })
+        
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    }
+
+}
