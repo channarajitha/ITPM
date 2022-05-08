b7acfae059050d7590da702de3b49ecddbe83216
IT20172596 <86620517+channarajitha@users.noreply.github.com>
2022-05-08 19:38:28 +0530

    Create HeaderData.js

---
+++ frontend/src/components/HeaderData.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import AccountCircleSharpIcon from '@material-ui/icons/AccountCircleSharp';
+import InsertChartIcon from '@material-ui/icons/InsertChart';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+
+export const  HeaderData =  [ 
+    
+    
+    {
+        title: "Administration ",
+        icon:<InsertChartIcon/>,
+        link: "/allsup",
+
+    },
+   
+    
+   
+    {
+        title: "Pandemic Management",
+        icon:<TrendingUpIcon/>,
+        link:"/all"
+
+    },
+    
+    {
+        title: "User Management",
+        icon:<LocalShippingIcon/>,
+        link:"/allret"
+
+    },
+    
+    {
+        title: "Leave Management",
+        icon:<SupervisedUserCircleIcon/>,
+        link:"/"
+
+    },
+   
+    {
+        title: "Attendance Management",
+        icon:<AirplanemodeActiveIcon/>,
+        link:"/allemp"
+
+    },
+   
+   
+
+
+
+
+];
+
+
+  
