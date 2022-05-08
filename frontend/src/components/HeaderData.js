1f808a2541f99e80aadb6899d2dfcfde247e8f1e
Pasindu Rathnayaka <88796335+Pasindurathnayaka@users.noreply.github.com>
2022-05-08 21:26:07 +0530

    side bar
    side bar

---
+++ frontend/src/components/HeaderData.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import AccountCircleSharpIcon from '@material-ui/icons/AccountCircleSharp';
+import InsertChartIcon from '@material-ui/icons/InsertChart';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+
+export const  HeaderData =  [ 
+    
+    
+    {
+        title: "Requst a Leave ",
+        icon:<InsertChartIcon/>,
+        link: "/add",
+
+    },
+   
+    
+   
+    {
+        title: "Leave Details",
+        icon:<TrendingUpIcon/>,
+        link:"/all"
+
+    },
+    
+    
+
+
+
+
+];
+
+
+  
